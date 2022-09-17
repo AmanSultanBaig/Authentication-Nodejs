7210b88c076f33ef87a2a4df7e9a25d9762305d4
AmanSultanBaig <amansultanbaig@gmail.com>
2022-09-17 22:43:39 +0500

    express-session added

--- server/index.js
+++ server/index.js
@@ -4,11 +4,21 @@ const dbConnection = require("./db/connection")
 const routes = require("./routes/auth")
 const { notFound, errorHandler } = require("./middlewares/error-handling")
 const passport = require('passport');
+const session = require("express-session")
 require('dotenv').config()
 require("./helper/passport")
 
+const { secretKey, DB_CONNECTION_STRING, PORT } = process.env;
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes)
+
+app.use(session({
+    secret: secretKey,
+    saveUninitialized: true,
+    resave: false
+}))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -16,11 +26,11 @@ app.use(passport.session());
 app.use(errorHandler) // global error handling | middlerware
 app.use(notFound) // if routes does'nt exist | error handling
 
-let portNo = 4040 || process.env.PORT
+let portNo = 4040 || PORT
 
 const startServer = async () => {
     try {
-        await dbConnection(process.env.DB_CONNECTION_STRING)
+        await dbConnection(DB_CONNECTION_STRING)
         console.log("DB Connection Established Successfully!")
         app.listen(portNo, __ => console.log(`App is running on http://localhost:${portNo}`))
     } catch (error) {
