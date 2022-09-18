4ba3b50f38d769e955a2c1a0babde87e72e9bbd1
AmanSultanBaig <amansultanbaig@gmail.com>
2022-09-18 21:44:16 +0500

    facebook login done | stored user to db | generated login token

--- server/helper/passport.js
+++ server/helper/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const UserModel = require("../models/auth.model")
+const { hashPassword } = require("../helper/bcrypt")
+const { createJwtToken } = require("../helper/jwt")
 
-const { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, BACKEND_URL } = process.env;
+const { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, BACKEND_URL, LOGIN_SECRET_KEY } = process.env;
 
 passport.serializeUser(function (user, cb) {
     cb(null, user);
@@ -15,6 +18,33 @@ passport.use(new FacebookStrategy({
     clientSecret: FACEBOOK_CLIENT_SECRET,//The App Secret generated when app was created on https://developers.facebook.com/
     callbackURL: `${BACKEND_URL}/callback`,
     profileFields: ['id', 'email', 'displayName'] // You have the option to specify the profile objects you want returned
-}, function (accessToken, refreshToken, profile, done) {
-    return done(null, profile);
+}, async function (accessToken, refreshToken, profile, done) {
+    const fbUserDetails = profile._json;
+    let response = {}
+
+    let isUserExist = await UserModel.findOne({ facebook_id: fbUserDetails.id, email: fbUserDetails.email })
+
+    if (isUserExist) {
+        const fb_loginToken = createJwtToken({ id: isUserExist._id }, LOGIN_SECRET_KEY, "7d");
+        response = {
+            token: fb_loginToken,
+            user: { email: isUserExist.email, name: isUserExist.name, id: isUserExist._id }
+        }
+        return done(null, response)
+    }
+    let hashedPassword = await hashPassword(fbUserDetails.id)
+
+    const body = {
+        email: fbUserDetails.email,
+        name: fbUserDetails.name,
+        facebookId: fbUserDetails.id,
+        password: hashedPassword
+    }
+
+    response = {
+        token: accessToken,
+        user: { email: fbUserDetails.email, name: fbUserDetails.name, id: fbUserDetails.id }
+    }
+    await UserModel.create(body)
+    return done(null, response);
 }))
