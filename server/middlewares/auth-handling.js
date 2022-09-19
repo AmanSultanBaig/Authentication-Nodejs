0c9c92590df678075c5141004acbd61f3da26f77
Suleman Khan <amansultanbaig@gmail.com>
2022-09-19 00:31:13 -0700

    authorization checking midleware added

--- server/middlewares/auth-handling.js
+++ server/middlewares/auth-handling.js
@@ -24,6 +24,34 @@ const authenticateUser = async (req, res, next) => {
     next()
 }
 
+const authorizedUser = async (req, res, next) => {
+    const { authorization } = req.headers
+    const roles = ["admin", "user"] 
+
+    if (!authorization) {
+        return res.status(401).json({ status: false, message: "Token required" })
+    }
+
+    const token = authorization.split(" ")[1]; // get token only skip "bearer"
+    const decodeToken = jwtTokenVerification(token, LOGIN_SECRET_KEY); // verify token given by user
+
+    if (!decodeToken) {
+        return res.status(401).json({ status: false, message: "Invalid or token expired, Please login again!" })
+    }
+
+    const userFound = await UserModel.findOne({ _id: decodeToken.id })
+    if (!userFound) {
+        return res.status(404).json({ status: false, message: "Invalid user request to login" })
+    }
+
+    if (!roles.includes(userFound.role)) {
+        return res.status(401).json({ status: false, message: "You don't have access to this api"})
+    }
+
+    next()
+}
+
 module.exports = {
-    authenticateUser
+    authenticateUser,
+    authorizedUser
 }
