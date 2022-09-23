17b5e2bfcaf7c533174a4c05c2691c18d97fd67b
AmanSultanBaig <amansultanbaig@gmail.com>
2022-09-23 22:20:08 +0500

    repository method added

--- server/repositories/user.repository.js
+++ server/repositories/user.repository.js
@@ -2,12 +2,64 @@ const userModel = require("../models/user.model")
 
 class UserRepository {
 
-    async getDocumentByColumns(params) {
-        const data = await userModel.findOne(params);
-        if (!data) {
-            return { status: false, message: "No record found", data }
+    async getSingleUser(params) {
+        try {
+            const data = await userModel.findOne(params);
+            if (!data) {
+                return null
+            }
+            return data
+        } catch (error) {
+            return error
         }
-        return { status: false, message: "Record found", data }
+    }
+
+    async getAllUsers() {
+        try {
+            const data = await userModel.find();
+            if (!data) {
+                return null
+            }
+            return data
+        } catch (error) {
+            return error
+        }
+    }
+
+    async createUser(params) {
+        try {
+            const data = await userModel.save(params);
+            if (!data) {
+                return null
+            }
+            return data
+        } catch (error) {
+            return error
+        }
+    }
+
+    async updateUser(id, params) {
+        try {
+            const data = await userModel.findByIdAndUpdate(id, params);
+            if (!data) {
+                return null
+            }
+            return data
+        } catch (error) {
+            return error
+        }
+    }
+
+    async removeUser(id) {
+        try {
+            const data = await userModel.findByIdAndDelete(id);
+            if (!data) {
+                return null
+            }
+            return data
+        } catch (error) {
+            return error
+        }
     }
 
 }
